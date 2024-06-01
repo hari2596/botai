3cf81a9e42d01d06542a92503cb59da3e60b5097
hari2596 <>
2024-06-02 00:17:50 +0530

    Added Hamburger Component and css

---
+++ src/Components/Hamburger/Hamburger.jsx
@@ -0,0 +1,13 @@
+import './Hamburger.css';
+
+const Hamburger = () => {
+    return (
+        <div className="hamburger-container">
+            <div className="hamburger-row"></div>
+            <div className="hamburger-row"></div>
+            <div className="hamburger-row"></div>
+        </div>
+    );
+}
+
+export default Hamburger;
